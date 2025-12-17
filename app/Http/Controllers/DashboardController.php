0ff23baccd9909d35bfdb13beca32da942b5de59
websncl <sncl0010@websncl.com>
2025-12-17 08:26:25 +0530

    user level check

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -529,7 +529,7 @@ class DashboardController extends Controller
     public function table()
     {
         $user = Auth::user();
-        $tableData = $this->flattenBinaryTree($this->buildBinaryTree($user->id));
+        $tableData = $this->flattenBinaryTree($this->buildBinaryTree($user->id, 15));
         
         return view('dashboard.table', [
             'user' => $user,
